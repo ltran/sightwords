389c516f0072f227e4e1b11331c73450de68d692
Louis Tran <tran.louis@gmail.com>
2017-09-11 21:40:03 -0700

    First pass at sightwords, need to fix compile error with release flag

--- src/sightwords.cr
+++ src/sightwords.cr
@@ -1,5 +1,22 @@
 require "./sightwords/*"
+require "kemal"
+
+macro renderer(filename)
+  render "src/views/#{{{filename}}}.ecr", "src/views/layouts/layout.ecr"
+end
 
 module Sightwords
-  # TODO Put your code here
+  # Matches GET "http://host:port/"
+  get "/" do
+    words = File.read_lines("src/sightwords.txt")
+
+    renderer "index"
+  end
+
+  # Creates a WebSocket handler.
+  # Matches "ws://host:port/socket"
+  #ws "/socket" do |socket|
+    #socket.send "Hello from Kemal!"
+  #end
 end
+Kemal.run
